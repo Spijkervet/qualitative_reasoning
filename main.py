6dc57ac7e6b50e98365a923bfa57eb3830ba4fa3
Tim <groeten.aan.tim@hotmail.com>
2019-03-22 12:52:23 +0100

    start of getting all the states and made a better framework

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import copy
+import numpy as np
+
 class Node:
 
     def __init__(self, nodeId, inflow, outflow, volume, relation): 
@@ -5,41 +8,62 @@ class Node:
         self.inflow = inflow
         self.outflow = outflow
         self.volume = volume
-        self.relation = []
+        self.relation = relation
 
     def __repr__(self):
         return str([self.nodeId, self.inflow, self.outflow, self.volume, self.relation])
 
-    def createRelation(self, nodes, relations):
-        for i, rela in enumerate(relations):
-            self.relation += [Dependencie(self.nodeId, nodes[i].nodeId, rela)]
-        print(self.relation)
-
-class Dependencie:
-
-    def __init__(self, depOf, depOn, relation):
-        self.depOf = depOf
-        self.depOn = depOn
-        self.relation = relation
-
-    def __repr__(self):
-        return "Node "+ str(self.depOf)+ " has relation "+ str(self.relation)+ " with " + str(self.depOn)
+class States:
+    def __init__(self, nodes, der):
+        # Creating a graph with nodes by 2 where the first is the volume
+        # and the second is the derivative. "start state"
+        self.states = np.zeros((len(nodes.nodes),2),dtype=np.int).tolist()
+        self.storedstates = [copy.deepcopy(self.states)]
+        self.changeState(nodes,der)
         
-class LoadSystem:
+    def changeState(self, nodes, der):
+        self.states[der][1] = '+'
+        while (self.storedstates[-1] != self.states):
+            self.storedstates += [copy.deepcopy(self.states)]
+            print(self.storedstates)
+            self.calcState(nodes)
 
-    def __init__(self, grid):
-        self.nodes = []
-        self.createNodes(grid)
-        self.insertRelations(grid)
+    def calcState(self, nodes):
+        for ind, item in enumerate(self.states):
+            if(item[0] == "+"):
+                self.reinforceRela(nodes.nodes[ind].relation, nodes)
+            elif(item[1] == "+"):
+                self.states[ind][0] = "+"
+
+    def reinforceRela(self, relation, nodes):
+        for ind, item in enumerate(relation[0]):
+            if (item == '+'):
+                self.states[ind][1] = "+"
 
     
+    def calcRule(item1,item2):
+        if (item1 == '+'):
+            if (item2 == '-' or item2 == '?'):
+                return '?'
+            else:
+                return '+'
+
+class LoadSystem:
+    # relaGrid is a number of nodes by number of nodes grid reading from 
+    # right to up the relations between the nodes. 
+    # capGrid is a grid by max inflow min inflow max volume min volume
+    def __init__(self, relaGrid):
+        self.nodes = []
+        self.createNodes(relaGrid)
+        for i in range(0, len(self.nodes)):
+            States(self,i)
+
     def createNodes(self, grid):
         for i in range(0,len(grid)):
-            self.nodes += [Node(i,0,0,0,[])]
-    
-    def insertRelations(self, grid):
-        for i, relas in enumerate(grid):
-            self.nodes[i].createRelation(self.nodes, relas)
+            nodeI = Node(i,0,0,0,[grid[i]])
+            print(nodeI)
+            self.nodes += [nodeI]
+
     
 
-LoadSystem([["+","-","+"],["0","-","+"],["-","-","-"]])
+LoadSystem([["0","+","0"],["0","0","0"],["0","+","0"]])
