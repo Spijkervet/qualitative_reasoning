6bf22ab79b8371c0e2dede3b0be8f821f546f34c
Janne Spijkervet <janne.spijkervet@gmail.com>
2019-04-08 10:37:25 +0200

    Foutje van tim <3

--- main.py
+++ main.py
@@ -10,19 +10,17 @@ if __name__ == '__main__':
             Model("I+", None, ("Tab", "Inflow"), ("Bathtub", "Volume")),
             Model("I-", None, ("Drain", "Outflow"), ("Bathtub", "Volume")),
             Model("P+", None, ("Container", "Volume"), ("Drain", "Outflow")),
-                Model("VC", MagnitudeValues.MAX, ("Container", "Volume"), ("Drain", "Outflow")),
-                Model("VC", MagnitudeValues.ZERO, ("Container", "Volume"), ("Drain", "Outflow")),
-                Model("VC", MagnitudeValues.MAX, ("Drain", "Outflow"), ("Container", "Volume")),
-                Model("VC", MagnitudeValues.ZERO, ("Drain", "Outflow"), ("Container", "Volume"))
+            Model("VC", MagnitudeValues.MAX, ("Container", "Volume"), ("Drain", "Outflow")),
+            Model("VC", MagnitudeValues.ZERO, ("Container", "Volume"), ("Drain", "Outflow")),
+            Model("VC", MagnitudeValues.MAX, ("Drain", "Outflow"), ("Container", "Volume")),
+            Model("VC", MagnitudeValues.ZERO, ("Drain", "Outflow"), ("Container", "Volume"))
         ]
 
+    states = generate()
+    new_states = clean_states(states)
+    print('new states: {}, old states {}'.format(len(new_states), len(states)))
 
-        print('main')
-        states = generate()
-        new_states = clean_states(states)
-        print('new states: {}, old states {}'.format(len(new_states), len(states)))
-
-        transitions = list(product(states, states))
-        new_transitions = clean_transitions(transitions)
-        print('new transitions: {}, old transitions {}'.format(len(new_transitions), len(transitions)))
+    transitions = list(product(states, states))
+    new_transitions = clean_transitions(transitions)
+    print('new transitions: {}, old transitions {}'.format(len(new_transitions), len(transitions)))
 
